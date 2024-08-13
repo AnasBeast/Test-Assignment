d1d86f96799b81afc3a3546d1137236b50b69883
AnasBeast <boussehminea@gmail.com>
2024-08-13 17:20:39 +0100

    btn + checkbox

---
+++ src/components/button.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Button() {
+  return (
+    <div>
+        <button 
+            type="button" 
+            class=" bg-[#FFCE22] hover:bg-[#FFD84D] font-medium rounded-lg text-sm w-[340px] h-[40px] me-2 mb-2 "
+        >
+        Done
+        </button>
+    </div>
+  )
+}
