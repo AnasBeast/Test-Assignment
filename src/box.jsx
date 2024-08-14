06d6bbf0c1a0dda107621639b1af8d45331f8733
AnasBeast <boussehminea@gmail.com>
2024-08-14 01:39:59 +0100

    comments

--- src/box.jsx
+++ src/box.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import Button from './components/button'
 
 export default function Box() {
-    const pages = ["Page 1","Page 2","Page 3","Page 4"]
+    {/* Inserting Pages Names */}
+    const pages = ["Page 1","Page 2","Page 3","Page 4"];
+
     return (
         <div className='shadow-[0_8px_15px_0px_rgba(20,20,20,0.12)] w-[410px] border rounded-lg text-[16px] font-light'>
             <div className='text-left p-4'>
@@ -12,6 +14,7 @@ export default function Box() {
                 </div>
                 <hr />
                 <div className='flex flex-col gap-5 py-5'>
+                    {/* Loop through pages */}
                     {pages.map((page)=>(
                         <div className='flex items-center justify-between'>
                             <label className='px-2'>{page}</label>
